074f129e063242b3f637c68117afde922361df09
SabriFevzi <58466802+SabriFevzi@users.noreply.github.com>
2022-09-04 15:46:57 +0300

    Add files via upload

---
+++ Socket.py
@@ -0,0 +1,16 @@
+#c sharp ve python kodlarını birleştirmek için kullanılan kod
+import socket
+
+def SendData(value):
+    for i in range(0,1):
+        host = "127.0.0.1"
+        port = 3029
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.sendall(bytes(str(value), encoding='utf-8'))
+        data = s.recv(1024)
+        s.close()
+        s.release()
+
+        return repr(data)
+
